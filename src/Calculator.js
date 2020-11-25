9db816515487afa2d802a084148b37e9456c1f76
Jeremy Langevin <langevinj832@gmail.com>
2020-11-25 08:29:30 -0500

    Calculator component built

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom'
 
 function Calculator(){
+    const history = useHistory();
+    const [nums, setNums] = useState({
+        num1: "",
+        num2: ""
+    });
 
+    handleChange = (evt) =>{
+        const { name, value } = evt.target
+        setNums(oldNums => ({
+            ...oldNums,
+            [name]: value
+        }));
+    }
+
+    handleSubmit = (evt) => {
+        evt.preventDefault();
+        const operation = evt.target.id
+        history.push(`/calc/${operation}/${nums.num1}/${nums.num2}`);
+    }
+
+    return (
+        <div className="Calculator">
+            <form onSubmit={handleSubmit}>
+                <div className="num1-div">
+                    <input type="text" name="num1" id="num1" value={nums.num1} placeholder="First Number" onChange={handleChange}></input>
+                </div>
+                <div className="num2-div">
+                    <input type="text" name="num2" id="num2" value={nums.num2} placeholder="Second Number" onChange={handleChange}></input>
+                </div>
+                <div className="operations">
+                    <button className="operator" id="add">+</button>
+                    <button className="operator" id="subtract">-</button>
+                    <button className="operator" id="multiply">x</button>
+                    <button className="operator" id="divide">/</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Calculator;
